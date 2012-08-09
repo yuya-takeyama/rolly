7beae38a57b1a60c00f49aa3ad6c8416447d6e5b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-09 10:40:22 +0900

    Remove garbage

--- src/Rolly/ActiveIf.php
+++ src/Rolly/ActiveIf.php
@@ -3,8 +3,6 @@
  * This file is part of Rolly.
  *
  * (c) Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
- * hlsearch
- 
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
